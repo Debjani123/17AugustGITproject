45d458c558a1171f86bfc6f51a762f9e6a3c4ac0
Debjani123 <debjanichatterjee8@gmail.com>
2021-08-18 13:26:26 +0530

    success

---
+++ src/GitProject/GitProject_Practice.java
@@ -0,0 +1,12 @@
+ package GitProject;
+
+public class GitProject_Practice {
+public static void main(String[] args) {
+	
+System.out.println("GIT codes");
+System.out.println("GIT commands");
+System.out.println("GIT data");
+System.out.println("GIT only");
+	
+}
+}
